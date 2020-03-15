10491c50c6104af725445b5173e71701a48beeb3
Meegan1 <meegan118@gmail.com>
2020-03-15 16:38:57 +0000

    Add project files.

---
+++ Inverse Kinematics/main.cpp
@@ -0,0 +1,10 @@
+#include <QApplication>
+#include "Rendering/Engine.h"
+
+int main(int argc, char **argv) {
+    QApplication app(argc, argv);
+    Engine engine(NULL);
+    engine.resize(600, 600);
+    engine.show();
+    return app.exec();
+}
